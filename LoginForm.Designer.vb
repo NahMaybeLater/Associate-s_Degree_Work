a040f974628fef243515c65a3d9f9986678d110e
Vivian L. <vivian.lapis@gmail.com>
2025-10-20 12:56:46 -0700

    Add files via upload

---
+++ LoginForm.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class LoginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim btnCancel As System.Windows.Forms.Button
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCancel
+        '
+        btnCancel.Location = New System.Drawing.Point(255, 32)
+        btnCancel.Name = "btnCancel"
+        btnCancel.Size = New System.Drawing.Size(75, 23)
+        btnCancel.TabIndex = 5
+        btnCancel.Text = "Cancel"
+        btnCancel.UseVisualStyleBackColor = True
+        AddHandler btnCancel.Click, AddressOf Me.btnCancel_Click
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(255, 4)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 0
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Location = New System.Drawing.Point(12, 9)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(58, 13)
+        Me.lblUsername.TabIndex = 1
+        Me.lblUsername.Text = "Username:"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(14, 37)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(56, 13)
+        Me.lblPassword.TabIndex = 2
+        Me.lblPassword.Text = "Password:"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(76, 6)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(173, 20)
+        Me.txtUsername.TabIndex = 3
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(76, 34)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(173, 20)
+        Me.txtPassword.TabIndex = 4
+        '
+        'LoginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 96)
+        Me.Controls.Add(btnCancel)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Name = "LoginForm"
+        Me.Text = "Please Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+End Class
---
+++ LoginForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class LoginForm
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        MessageBox.Show("Thank you for logging in, " & txtUsername.Text, "Logged In.")
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs)
+        Application.Exit()
+    End Sub
+
+    Private Sub txtUsername_TextChanged(sender As Object, e As EventArgs) Handles txtUsername.TextChanged
+
+    End Sub
+End Class
